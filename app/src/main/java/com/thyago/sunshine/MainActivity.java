2d787de0a2d71c2e291c5645d5cef40a9d330f3e
thyagostall <thstall@gmail.com>
2016-07-25 00:14:20 -0300

    Created fake data and list view

--- app/src/main/java/com/thyago/sunshine/MainActivity.java
+++ app/src/main/java/com/thyago/sunshine/MainActivity.java
@@ -7,6 +7,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,7 +32,22 @@ public class MainActivity extends AppCompatActivity {
         @Nullable
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_main, container, false);
+            ArrayList<String> fakeData = new ArrayList<>(7);
+            fakeData.add("Today - Sunny - 88/63");
+            fakeData.add("Tomorrow - Foggy - 70/46");
+            fakeData.add("Wed - Cloudy - 72/63");
+            fakeData.add("Thurs - Rainy - 64/51");
+            fakeData.add("Fri - Foggy - 70/46");
+            fakeData.add("Sat - Sunny - 76/68");
+            fakeData.add("Sun - Sunny - 80/66");
+
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, fakeData);
+
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(adapter);
+
+            return rootView;
         }
     }
 }
