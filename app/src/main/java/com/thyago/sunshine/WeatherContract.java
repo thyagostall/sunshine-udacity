fd2c513fe490820289c9137209978b9cf1c3b2c3
thyagostall <thstall@gmail.com>
2016-08-01 00:55:17 -0300

    Weather Contract

---
+++ app/src/main/java/com/thyago/sunshine/WeatherContract.java
@@ -0,0 +1,34 @@
+package com.thyago.sunshine;
+
+import android.provider.BaseColumns;
+
+import java.sql.Time;
+
+/**
+ * Created by thyago on 8/1/16.
+ */
+public class WeatherContract {
+    public static final class LocationEntry implements BaseColumns {
+        public static final String TABLE_NAME = "location";
+
+        public static final String COLUMN_LOCATION_SETTING = "location_setting";
+        public static final String COLUMN_CITY_NAME = "city_name";
+        public static final String COLUMN_COORD_LAT = "coord_lat";
+        public static final String COLUMN_COORD_LONG = "coord_long";
+    }
+
+    public static final class WeatherEntry implements BaseColumns {
+        public static final String TABLE_NAME = "weather";
+
+        public static final String COLUMN_LOC_KEY = "location_id";
+        public static final String COLUMN_DATE = "date";
+        public static final String COLUMN_WEATHER_ID = "weather_id";
+        public static final String COLUMN_SHORT_DESC = "short_desc";
+        public static final String COLUMN_MIN_TEMP = "min";
+        public static final String COLUMN_MAX_TEMP = "max";
+        public static final String COLUMN_HUMIDITY = "humidity";
+        public static final String COLUMN_PRESSURE = "pressure";
+        public static final String COLUMN_WIND_SPEED = "wind";
+        public static final String COLUMN_DEGREES = "degrees";
+    }
+}
