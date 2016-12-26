39c297094e451932c4ec817866ad19829f7587e6
thyagostall <thstall@gmail.com>
2016-12-26 21:57:26 -0200

    Added dynamic dimensions for different sizes

--- app/src/main/java/com/example/android/sunshine/app/DetailFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailFragment.java
@@ -49,7 +49,6 @@ public class DetailFragment extends Fragment implements LoaderManager.LoaderCall
 
     private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
 
-    private ShareActionProvider mShareActionProvider;
     private String mForecast;
     private Uri mUri;
 
@@ -109,7 +108,7 @@ public class DetailFragment extends Fragment implements LoaderManager.LoaderCall
             mUri = arguments.getParcelable(DetailFragment.DETAIL_URI);
         }
 
-        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_detail_start, container, false);
         mIconView = (ImageView) rootView.findViewById(R.id.detail_icon);
         mDateView = (TextView) rootView.findViewById(R.id.detail_date_textview);
         mDescriptionView = (TextView) rootView.findViewById(R.id.detail_forecast_textview);
@@ -173,13 +172,15 @@ public class DetailFragment extends Fragment implements LoaderManager.LoaderCall
                     null
             );
         }
+        getView().setVisibility(View.INVISIBLE);
         return null;
     }
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
         if (data != null && data.moveToFirst()) {
-            // Read weather condition ID from cursor
+            getView().setVisibility(View.VISIBLE);
+
             int weatherId = data.getInt(COL_WEATHER_CONDITION_ID);
 
             if (Utility.isUsingLocalGraphics(getActivity())) {
@@ -240,11 +241,6 @@ public class DetailFragment extends Fragment implements LoaderManager.LoaderCall
 
             // We still need this for the share intent
             mForecast = String.format("%s - %s - %s/%s", dateText, description, high, low);
-
-            // If onCreateOptionsMenu has already happened, we need to update the share intent now.
-            if (mShareActionProvider != null) {
-                mShareActionProvider.setShareIntent(createShareForecastIntent());
-            }
         }
 
         AppCompatActivity activity = (AppCompatActivity) getActivity();
